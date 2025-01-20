dc123cb393ee6bc3c0bb7a73b345ba63697f5ae7
Tzanislav <tzanislav@gmail.com>
2025-01-20 18:29:04 +0200

    User system 2

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Brands from './pages/brands';
-import Models3D from './pages/Models3D';
-import BrandForm from './components/BrandForm';
-import Model3DForm from './components/Model3DForm';
-import ExampleForm from './components/TestUploader';
-import ShowModel from './pages/ShowModel'; // Optional component to show a single model
+import Home from './pages/Home'; // Optional component to show a single model
 import Projects from './pages/Projects'; // Optional component to show projects
 import ProjectForm from './components/ProjectForm';
 import ShowProject from './pages/ShowProject'; // Optional component to show a single project
@@ -30,18 +24,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/items" element={<Items />} />
-          <Route
-            path="/brands"
-            element={<ProtectedRoute element={<Brands />} />}
-          />
-          <Route
-            path="/brands/:id"
-            element={<ProtectedRoute element={<BrandForm />} />}
-          />
-          <Route
-            path="/brands/new"
-            element={<ProtectedRoute element={<BrandForm />} />}
-          />
+        
           <Route
             path="/items/new"
             element={<ProtectedRoute element={<ItemForm />} />}
@@ -53,22 +36,6 @@ function App() {
           <Route
             path="/items/:id"
             element={<ProtectedRoute element={<ItemForm />} />}
-          />
-          <Route
-            path="/models3d"
-            element={<ProtectedRoute element={<Models3D />} />}
-          />
-          <Route
-            path="/models3d/:id"
-            element={<ProtectedRoute element={<ShowModel />} />}
-          />
-          <Route
-            path="/models3d/edit/:id"
-            element={<ProtectedRoute element={<Model3DForm />} />}
-          />
-          <Route
-            path="/models3d/new"
-            element={<ProtectedRoute element={<Model3DForm />} />}
           />
           <Route
             path="/projects"
@@ -89,10 +56,6 @@ function App() {
           <Route
             path="/selections/:id"
             element={<ProtectedRoute element={<ShowSelection />} />}
-          />
-          <Route
-            path="/test"
-            element={<ProtectedRoute element={<ExampleForm />} />}
           />
         </Routes>
         </ActiveSelectionProvider>
